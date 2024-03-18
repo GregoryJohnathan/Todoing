6c74977e06c68c88bb8945ee3ed228250570c60f
GregoryJohnathan <112404410+GregoryJohnathan@users.noreply.github.com>
2024-03-18 10:15:45 -0400

    Selection working

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,13 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> items;
+    private ArrayList<Boolean> itemsSelection;
     private ListView list;
     private Button button;
     private ArrayAdapter<String> itemsAdapter;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         list = findViewById(R.id.list);
         button = findViewById(R.id.button);
 
+
         // Apparently THIS is how you create a new item with button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         items = new ArrayList<>();
+        itemsSelection = new ArrayList<>();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         list.setAdapter(itemsAdapter);
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -48,15 +54,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                checkOffItem(position);
-            }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+                checkOffItem(position, view);
             }
         });
 
@@ -67,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         Toast.makeText(context, "Yo task just got: DELETED", Toast.LENGTH_LONG).show();
         items.remove(position);
+        itemsSelection.remove(position);
         itemsAdapter.notifyDataSetChanged();
         return true;
     }
@@ -78,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         if(!(itemText.equals(""))){
             itemsAdapter.add(itemText);
             input.setText("");
+            itemsSelection.add(false);
         }
 
         else{
@@ -86,10 +90,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void checkOffItem(int position){
-
+    private void checkOffItem(int position, View view){
+        EditText input = findViewById(R.id.edit_text);
         Context context = getApplicationContext();
-        Toast.makeText(context, "Yo task just got: CHECKED", Toast.LENGTH_SHORT).show();
+
+
+        if(!itemsSelection.get(position)){
+            Toast.makeText(context, "Yo task just got: CHECKED", Toast.LENGTH_SHORT).show();
+            view.setBackgroundColor(Color.GRAY);
+        } else{
+            Toast.makeText(context, "Yo task just got: UNCHECKED", Toast.LENGTH_SHORT).show();
+            view.setBackgroundColor(Color.WHITE);
+        }
+        itemsSelection.set(position, !itemsSelection.get(position));
+
+        //itemsAdapter.getItem(position)
         itemsAdapter.notifyDataSetChanged();
 
     }
